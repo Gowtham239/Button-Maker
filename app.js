98d37e957c2ff6231b48beacd88e6fb078ad5b90
Gowtham Raju <gowthamraju239@gmail.com>
2024-10-08 19:10:25 +0530

    add getuserInputValues() and eventLister click

--- app.js
+++ app.js
@@ -6,10 +6,28 @@ const paddingInPx = document.querySelector("#padding-in-px");
 const borderRadiusInPx = document.querySelector("#border-radius-in-px");
 const applyBtn = document.querySelector("#apply-btn");
 const resultBtn = document.querySelector("#result-btn");
+const errorMessage = document.querySelector(".error-message")
 
 function userInputValues() {
-    
-    backgroundColor.value
+    if(backgroundColor.value === "" 
+        || fontColor.value === "" 
+        || fontSize.value === ""
+        || fontWeight.value === ""
+        || paddingInPx.value === ""
+        || borderRadiusInPx.value === "") {
+            errorMessage.textContent = "Inputs should not be empty";
+            errorMessage.style.color = "red";
+        } else {
+            resultBtn.style.backgroundColor =  backgroundColor.value;
+            resultBtn.style.color = fontColor.value;
+            resultBtn.style.fontSize = fontSize.value;
+            resultBtn.style.fontWeight = fontWeight.value;
+            resultBtn.style.padding = paddingInPx.value;
+            resultBtn.style.borderRadius = borderRadiusInPx.value;
+            resultBtn.style.border = "none";
+
+            errorMessage.textContent = "";
+        }  
 }
 
 applyBtn.addEventListener("click", userInputValues);
