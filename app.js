8029b9f177ebee6184b41ab3113630b2558dc02c
Gowtham Raju <gowthamraju239@gmail.com>
2024-10-08 18:43:03 +0530

    add a function to get all the user inputs

---
+++ app.js
@@ -0,0 +1,16 @@
+const backgroundColor = document.querySelector("#background-color");
+const fontColor = document.querySelector("#font-color");
+const fontSize = document.querySelector("#font-size");
+const fontWeight = document.querySelector("#font-weight");
+const paddingInPx = document.querySelector("#padding-in-px");
+const borderRadiusInPx = document.querySelector("#border-radius-in-px");
+const applyBtn = document.querySelector("#apply-btn");
+const resultBtn = document.querySelector("#result-btn");
+
+function userInputValues() {
+    
+    backgroundColor.value
+}
+
+applyBtn.addEventListener("click", userInputValues);
+
